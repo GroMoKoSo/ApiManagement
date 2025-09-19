e42f069126634f693a636a5b34c52c5208f94451
Justin Wolek <juwol1423@gmail.com>
2025-09-19 14:51:30 +0200

    feat: add service interface as well as its implementation
    Refs: #3

---
+++ src/main/java/de/thm/apimanagement/service/ApiService.java
@@ -0,0 +1,71 @@
+package de.thm.apimanagement.service;
+
+import de.thm.apimanagement.entity.Api;
+import de.thm.apimanagement.entity.InvokeQuery;
+import de.thm.apimanagement.entity.InvokeResult;
+
+import java.util.List;
+
+/**
+ * Defines an API service which contains the business logic of the microservice.
+ * Meant to be used by a RESTController
+ *
+ * @author Justin Wolek
+ */
+public interface ApiService {
+
+    /**
+     * Handles saving an API
+     *
+     * @param api   The API to save
+     * @return      The saved API
+     */
+    public Api saveApi(Api api);
+
+    /**
+     * Handles updating an API
+     *
+     * @param apiId The id of the API to update
+     * @param api   The API object which should be used to update its current instance
+     * @return      The updated API
+     */
+    public Api updateApi(int apiId, Api api);
+
+    /**
+     * Handles deleting an API
+     *
+     * @param apiId The id of the API which should be deleted
+     */
+    public void deleteApiById(int apiId);
+
+    /**
+     * Gets an array which contains all APIs
+     *
+     * @return  An array with every current API
+     */
+    public List<Api> fetchApiList();
+
+    /**
+     * Gets one {@link Api} or {@code null} with a matching id.
+     *
+     * @param apiId The id of the API which should be fetched.
+     * @return      The {@link Api} with a matching id or {@code null}
+     */
+    public Api fetchApiById(int apiId);
+
+    /**
+     * Handles querying an HTTP request to an API according to the {@link InvokeQuery}
+     *
+     * @param query The query which should be performed
+     * @return      A {@link InvokeResult} which wraps the HTTP response
+     */
+    public InvokeResult invoke(InvokeQuery query);
+
+    /**
+     * Takes an {@link InvokeQuery} and returns a string with the URL.
+     *
+     * @param query The query which should be converted
+     * @return      A string, containing the request target URL
+     */
+    public String formatRequestPath(InvokeQuery query);
+}
---
+++ src/main/java/de/thm/apimanagement/service/ApiServiceImpl.java
@@ -0,0 +1,119 @@
+package de.thm.apimanagement.service;
+
+import de.thm.apimanagement.client.Spec2ToolClient;
+import de.thm.apimanagement.client.UserManagementClient;
+import de.thm.apimanagement.entity.Api;
+import de.thm.apimanagement.entity.InvokeQuery;
+import de.thm.apimanagement.entity.InvokeResult;
+import de.thm.apimanagement.repository.ApiRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Implementation of ApiService. Contains the business logic of the microservice
+ * Meant to be used by a RESTController
+ *
+ * @author Justin Wolek
+ */
+@Service
+public class ApiServiceImpl implements de.thm.apimanagement.service.ApiService {
+    private ApiRepository apiRepository;
+    private Spec2ToolClient spec2ToolClient;
+    private UserManagementClient userManagementClient;
+
+    ApiServiceImpl(ApiRepository apiRepository) {
+        this.apiRepository = apiRepository;
+        this.spec2ToolClient = new Spec2ToolClient("http://localhost:8000");
+        this.userManagementClient = new UserManagementClient("http://localhost:8000");
+    }
+
+    @Override
+    public Api saveApi(Api api) {
+        // TODO:
+        // - Check if user is authorized
+        // - When not authorized:
+        //      - return 401
+        // - When authorized:
+        //      - Call convertSpec2Tool()
+        //      - Call addTool()
+
+        return apiRepository.save(api);
+    }
+
+    @Override
+    public Api updateApi(int apiId, Api api) {
+        // TODO:
+        // - Check if user is authorized
+        // - When not authorized:
+        //      - return 401
+        // - When authorized:
+        //      - Call convertSpec2Tool()
+        //      - Call removeTool()
+        //      - Call addTool()
+
+        return apiRepository.findById(apiId)
+                .map(apiDb -> {
+                    if (api.getName() != null && !api.getName().isBlank()) {
+                        apiDb.setName(api.getName());
+                    }
+                    if (api.getDescription() != null && !api.getDescription().isBlank()) {
+                        apiDb.setDescription(api.getDescription());
+                    }
+                    if (api.getVersion() != null && !api.getVersion().isBlank()) {
+                        apiDb.setVersion(api.getVersion());
+                    }
+                    if (api.getDataFormat() != null && !api.getDataFormat().isBlank()) {
+                        apiDb.setDataFormat(api.getDataFormat());
+                    }
+                    if (api.getSpec() != null && !api.getSpec().isBlank()) {
+                        apiDb.setSpec(api.getSpec());
+                    }
+                    if (api.getToken() != null && !api.getToken().isBlank()) {
+                        apiDb.setToken(api.getToken());
+                    }
+                    return apiRepository.save(apiDb);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("API with id " + apiId + " not found"));
+    }
+
+    @Override
+    public void deleteApiById(int apiId) {
+        // TODO:
+        // - Check if user is authorized
+        // - When not authorized:
+        //      - return 401
+        // - When authorized:
+        //      - Call removeTool()
+
+        apiRepository.findById(apiId)
+                .ifPresentOrElse(
+                        apiRepository::delete,
+                        () -> { throw new EntityNotFoundException("API with id " + apiId + " not found"); }
+                );
+    }
+
+    @Override
+    public List<Api> fetchApiList() {
+        return (List<Api>) apiRepository.findAll();
+    }
+
+    @Override
+    public Api fetchApiById(int apiId) {
+        return apiRepository.findById(apiId)
+                .orElseThrow(() -> new EntityNotFoundException("API with id " + apiId + " not found"));
+    }
+
+    @Override
+    public InvokeResult invoke(InvokeQuery query) {
+        // TODO: Implement
+        return null;
+    }
+
+    @Override
+    public String formatRequestPath(InvokeQuery query) {
+        // TODO: Implement
+        return "";
+    }
+}
