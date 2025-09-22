7949079e7f0bd89cb3bf215959da35e2fcb8a290
Justin Wolek <juwol1423@gmail.com>
2025-09-22 07:56:24 +0200

    fix: add entity type

--- src/main/java/de/thm/apimanagement/repository/ApiRepository.java
+++ src/main/java/de/thm/apimanagement/repository/ApiRepository.java
@@ -1,5 +1,6 @@
 package de.thm.apimanagement.repository;
 
+import de.thm.apimanagement.entity.Api;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ApiRepository extends CrudRepository {}
+public interface ApiRepository extends CrudRepository<Api, Integer> {}
